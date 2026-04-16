feb93442a39237158ee27e5dc8efa286f2236f93
tomomini0815 <tomomini0815@gmail.com>
2026-04-16 13:59:34 +0900

    fix: add QueryClientProvider for @mysten/dapp-kit
    - Add @tanstack/react-query dependency
    - Wrap app with QueryClientProvider in main.tsx
    - Fix 'No QueryClient set' error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SuiClientProvider, WalletProvider, createNetworkConfig } from '@mysten/dapp-kit';
 import App from './App.tsx';
 import './index.css';
 import '@mysten/dapp-kit/dist/index.css';
+
+const queryClient = new QueryClient();
 
 const { networkConfig } = createNetworkConfig({
   mainnet: { url: 'https://fullnode.mainnet.sui.io:443' },
@@ -12,10 +15,12 @@ const { networkConfig } = createNetworkConfig({
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
-      <WalletProvider>
-        <App />
-      </WalletProvider>
-    </SuiClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
